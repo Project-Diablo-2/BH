656aad8113d63d8dbbda31059ce98c58c9b2f351
Alex <72409387+fergushev@users.noreply.github.com>
2023-01-22 21:50:43 -0700

    Fixed bug with SHOP causing other conds to break
    The 'inStore" flag here has absolutely nothing to do with the NPC shops. It's an attribute that gets set when an item first drops from a mob, chest, etc. So this was pretty much _always_ returning true.
    
    There's no reason to have a packet check for this condition anyway so we can just always return false
    
    Also added a few comments to make these attributes a bit more clear

--- BH/Modules/Item/ItemDisplay.cpp
+++ BH/Modules/Item/ItemDisplay.cpp
@@ -2605,7 +2605,7 @@ bool ShopCondition::EvaluateInternalFromPacket(ItemInfo* info,
 	Condition* arg1,
 	Condition* arg2)
 {
-	return IntegerCompare(info->inStore, (BYTE)EQUAL, true);
+	return false;
 }
 
 bool OneHandedCondition::EvaluateInternal(UnitItemInfo* uInfo,
