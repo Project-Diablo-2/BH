39aa6c2b43735c372ad1cfa42148e16165e6bda1
Canite <7261074+Canite@users.noreply.github.com>
2025-10-30 12:59:05 -0400

    Fixed a missing DLL enum for d2sound

--- BH/Patch.cpp
+++ BH/Patch.cpp
@@ -12,7 +12,7 @@ Patch::Patch(PatchType type, Dll dll, Offsets offsets, int function, int length)
 int Patch::GetDllOffset(Dll dll, int offset) {
 	const char* szDlls[] = {"D2CLIENT.dll", "D2COMMON.dll", "D2GFX.dll", "D2LANG.dll",
 							"D2WIN.dll", "D2NET.dll", "D2GAME.dll", "D2LAUNCH.dll",
-							"FOG.dll", "BNCLIENT.dll", "STORM.dll", "D2CMP.dll", "D2MULTI.dll", "D2MCPCLIENT.dll", "D2CMP.dll"};
+							"FOG.dll", "BNCLIENT.dll", "STORM.dll", "D2CMP.dll", "D2MULTI.dll", "D2MCPCLIENT.dll", "D2CMP.dll", "D2SOUND.dll"};
 	//Attempt to get the module of the given DLL
 	HMODULE hModule = GetModuleHandle(szDlls[dll]);
 
