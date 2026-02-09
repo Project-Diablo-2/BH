95d8a64fc8aa0a72906873a3916a7e60df50b2a7
exiledagain <98454299+exiledagain@users.noreply.github.com>
2026-02-09 07:46:48 -0500

    Add BASEBLOCK key

--- BH/Modules/Item/ItemDisplay.cpp
+++ BH/Modules/Item/ItemDisplay.cpp
@@ -518,6 +518,7 @@ enum FilterCondition
 	COND_ADD,
 	COND_TRUE,
 	COND_FALSE,
+	COND_BASEBLOCK,
 
 	COND_NULL
 };
@@ -684,6 +685,7 @@ std::map<std::string, FilterCondition> condition_map =
 	{"BUYPRICE", COND_BUYPRICE},
 	{"SELLPRICE", COND_PRICE},
 	{"PRICE", COND_PRICE},
+	{"BASEBLOCK", COND_BASEBLOCK},
 	// These have a number as part of the key, handled separately
 	//{"SK", COND_SK},
 	//{"OS", COND_OS},
@@ -912,6 +914,8 @@ struct ReplacementSpec {
 	static string ReplaceConditionalLine(ReplaceContext& ctx, const ReplacementValue& val);
 	// %NL%
 	static string ReplaceNewLine(ReplaceContext& ctx, const ReplacementValue& val);
+	// %BASEBLOCK%
+	static string ReplaceBaseBlock(ReplaceContext& ctx, const ReplacementValue& val);
 
 	// DYNAMIC keywords
 	// %STAT%
@@ -965,6 +969,7 @@ unordered_map<string, ReplacementSpec> ReplacementMap = {
 	{ "ED", { 0, ReplacementSpec::ReplaceEnhancedD } },
 	{ "CL", { 0, ReplacementSpec::ReplaceConditionalLine } },
 	{ "NL", { 0, ReplacementSpec::ReplaceNewLine } },
+	{ "BASEBLOCK", { 0, ReplacementSpec::ReplaceBaseBlock } },
 	// named stats
 	{ "EDEF", { 0, ReplacementSpec::ReplaceNamedStat(STAT_ENHANCEDDEFENSE) } },
 	{ "EDAM", { 0, ReplacementSpec::ReplaceNamedStat(STAT_ENHANCEDMAXIMUMDAMAGE) } },
@@ -1213,6 +1218,13 @@ string ReplacementSpec::ReplaceAllResist(ReplaceContext& ctx, const ReplacementV
 string ReplacementSpec::ReplaceEnhancedD(ReplaceContext& ctx, const ReplacementValue& val)
 {
 	return NameVarEd(ctx.info);
+}
+
+string ReplacementSpec::ReplaceBaseBlock(ReplaceContext& ctx, const ReplacementValue& val)
+{
+	char buffer[16];
+	snprintf(buffer, 16, "%d", BaseBlockCondition::GetValue(ctx.info));
+	return buffer;
 }
 
 string ReplacementSpec::ReplaceConditionalLine(ReplaceContext& ctx, const ReplacementValue& val)
@@ -2816,6 +2828,9 @@ void Condition::BuildConditions(vector<Condition*>& conditions,
 	case COND_ADD:
 		Condition::AddOperand(conditions, new AddCondition(key, operation, value));
 		break;
+	case COND_BASEBLOCK:
+		Condition::AddOperand(conditions, new BaseBlockCondition(operation, value, value2));
+		break;
 
 	case COND_NULL:
 		break;
@@ -3585,6 +3600,19 @@ bool AddCondition::EvaluateInternal(UnitItemInfo* uInfo,
 	}
 
 	return IntegerCompare(value, operation, targetStat);
+}
+
+int BaseBlockCondition::GetValue(UnitItemInfo* uInfo)
+{
+	auto txt = D2COMMON_GetItemText(uInfo->item->dwTxtFileNo);
+	return txt->bblock;
+}
+
+bool BaseBlockCondition::EvaluateInternal(UnitItemInfo* uInfo,
+	Condition* arg1,
+	Condition* arg2)
+{
+	return IntegerCompare(GetValue(uInfo), operation, targetStat, targetStat2);
 }
 
 int GetStatFromList(UnitItemInfo* uInfo, int itemStat)
